bba25cdfc3fd225f26fec050ed3b26aa97a6b910
Nick Hudak <nhudak@pentaho.com>
2017-06-29 11:44:02 -0400

    code-gen command api

--- command-api/src/main/java/com/github/hudak/vertx/examples/api/Command.java
+++ command-api/src/main/java/com/github/hudak/vertx/examples/api/Command.java
@@ -1,8 +1,14 @@
 package com.github.hudak.vertx.examples.api;
 
+import io.reactivex.disposables.Disposable;
+import io.reactivex.disposables.Disposables;
+import io.vertx.codegen.annotations.GenIgnore;
+import io.vertx.codegen.annotations.ProxyGen;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
+import io.vertx.core.eventbus.MessageConsumer;
+import io.vertx.core.json.JsonObject;
 import io.vertx.serviceproxy.ProxyHelper;
 
 import java.util.List;
@@ -10,10 +16,19 @@ import java.util.List;
 /**
  * Created by hudak on 6/28/17.
  */
+@ProxyGen
+//@FunctionalInterface
 public interface Command {
     void run(List<String> arguments, Handler<AsyncResult<Void>> handler);
 
+    @GenIgnore
     static Command createProxy(Vertx vertx, String address) {
         return ProxyHelper.createProxy(Command.class, vertx, address);
+    }
+
+    @GenIgnore
+    static Disposable registerService(Vertx vertx, Command instance, String address) {
+        MessageConsumer<JsonObject> consumer = ProxyHelper.registerService(Command.class, vertx, instance, address);
+        return Disposables.fromAction(() -> ProxyHelper.unregisterService(consumer));
     }
 }
---
+++ command-api/src/main/java/com/github/hudak/vertx/examples/api/package-info.java
@@ -0,0 +1,4 @@
+@ModuleGen(name = "example-api", groupPackage = "com.github.hudak.vertx.examples.api")
+package com.github.hudak.vertx.examples.api;
+
+import io.vertx.codegen.annotations.ModuleGen;
